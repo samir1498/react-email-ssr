f5b46315b01b021fc0d2da5084fca8e4cc334a04
Samir <samir@itchallenge.fr>
2025-02-18 03:48:07 +0100

    feat(react-email-ssr): initial setup + apple email template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AppleReceiptEmail from './templates/apple-receipt'
+
+function App() {
+
+  return (
+    <>
+      <AppleReceiptEmail />
+    </>
+  )
+}
+
+export default App
